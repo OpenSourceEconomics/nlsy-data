e4edd70244bd70dab01a13fcf70ba3a0f7d4d0d9
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2018-05-02 09:15:20 +0200

    intial organization

---
+++ presentation/create.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""This module compiles the notes with the figures."""
+import subprocess
+import shutil
+import os
+
+if __name__ == '__main__':
+
+    for task in ['pdflatex', 'bibtex', 'pdflatex', 'pdflatex']:
+        subprocess.check_call(task + ' main', shell=True)
+        
